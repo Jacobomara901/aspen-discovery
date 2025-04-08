858db204574ce0450227547d23d0a70d1942c6f0
Jacob O'Mara <jacob.omara@openfifth.co.uk>
2025-04-08 14:26:36 +0100

    mailer picker test
    use host name for all mail setting type descriptions

--- code/web/sys/DBMaintenance/version_updates/25.04.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.04.00.php
@@ -94,5 +94,14 @@ function getUpdates25_04_00(): array {
 
 		//other
 
+		'library_preferred_mail_settings' => [
+			'title' => 'Library Preferred Mail Settings',
+			'description' => 'Add field to store preferred mail settings for each library',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE library ADD COLUMN preferredMailSettingsId VARCHAR(50) DEFAULT NULL",
+			]
+		], //library_preferred_mail_settings
+
 	];
 }
