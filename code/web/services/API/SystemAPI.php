a09fbc6ba1eb626f62669eb579bafb84d3eae6e6
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2025-06-26 19:19:44 +0100

    fix: correct plugin database update file naming
    - Move plugin updates from version_updates to module updates
    - Create plugin_updates.php following Aspen module pattern
    - Register plugin updates in SystemAPI properly
    - Fix function naming convention to match Aspen standards

--- code/web/services/API/SystemAPI.php
+++ code/web/services/API/SystemAPI.php
@@ -391,8 +391,10 @@ class SystemAPI extends AbstractAPI {
 		$communityEngagementUpdates = getCommunityEngagementUpdates();
 		require_once ROOT_DIR . '/sys/DBMaintenance/talpa_updates.php';
 		$talpaUpdates = getTalpaUpdates();
+		require_once ROOT_DIR . '/sys/DBMaintenance/plugin_updates.php';
+		$pluginUpdates = getPluginUpdates();
 		
-		$baseUpdates = array_merge($library_location_updates, $summonUpdates, $cloudLibraryUpdates, $grapesWebBuilderUpdates, $communityEngagementUpdates, $talpaUpdates, $heycentricUpdates);
+		$baseUpdates = array_merge($library_location_updates, $summonUpdates, $cloudLibraryUpdates, $grapesWebBuilderUpdates, $communityEngagementUpdates, $talpaUpdates, $heycentricUpdates, $pluginUpdates);
 
 		//Get version updates
 		require_once ROOT_DIR . '/sys/Utils/StringUtils.php';
