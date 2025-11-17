e4286dd6e612ea3b1b52f2d6d8feee6ca9f35b7d
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2025-11-17 16:28:00 +0000

    DIS-1578: add 'eventsDefaultCalenderView' column to library table

--- code/web/sys/DBMaintenance/version_updates/25.12.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.12.00.php
@@ -28,11 +28,20 @@ function getUpdates25_12_00(): array {
 
 		//chloe - Open Fifth
 
+		//jacob - Open Fifth
+		'library_events_default_calendar_view' => [
+			'title' => 'Library Events Default Calendar View',
+			'description' => 'Add eventsDefaultCalendarView field to library table for filtering calendar by location',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE library ADD COLUMN eventsDefaultCalendarView TINYINT(1) DEFAULT 0 COMMENT '0 = All Locations, 1 = Use library's main branch location, 2 = Use first library location (alphabetically)'"
+			]
+		],
+
 		//James Staub - Nashville Public Library
 
 		//Lucas Montoya - Theke Solutions
 
 		//other
-		
 	];
 }
