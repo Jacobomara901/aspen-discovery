b658bf0772874444ac432495fe63149feca321bd
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2025-11-19 15:23:25 +0000

    limit view of events dates to 5 by default with locale support
    This amends the facet for "other dates in this series" to show 5 dates
    by default with a "more..." button to show all others. Otherwise the
    facets could be flooded if there were many dates in the series.
    This also adds the locale based handling to the date display

--- code/web/interface/themes/responsive/js/aspen/events.js
+++ code/web/interface/themes/responsive/js/aspen/events.js
@@ -668,6 +668,16 @@ AspenDiscovery.Events = (function(){
 					win.print();
 				};
 			}
+		},
+
+		lessEventDates: function() {
+			document.getElementById("moreEventDates").style.display = "block";
+			document.getElementById("narrowGroupHidden_eventDates").style.display = "none";
+		},
+
+		moreEventDates: function() {
+			document.getElementById("moreEventDates").style.display = "none";
+			document.getElementById("narrowGroupHidden_eventDates").style.display = "block";
 		}
 	};
 }(AspenDiscovery.Events || {}));
