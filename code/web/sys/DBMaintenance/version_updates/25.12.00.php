277e60923271fdc617dbc6d608e8c6c6c70bb113
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2025-11-19 11:10:04 +0000

    add parent permisison identifier to db

--- code/web/sys/DBMaintenance/version_updates/25.12.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.12.00.php
@@ -27,12 +27,22 @@ function getUpdates25_12_00(): array {
 		//alexander - Open Fifth
 
 		//chloe - Open Fifth
+    
+    //Jacob O'Mara - Open Fifth
+		'permission_groups_hierarchy' => [
+			'title' => 'Permission Groups Hierarchy',
+			'description' => 'Add support for hierarchical permission groups with parent-child relationships',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE permission_groups ADD COLUMN parentGroupKey VARCHAR(100) DEFAULT NULL AFTER description",
+			]
+		], //permission_groups_hierarchy
 
 		//James Staub - Nashville Public Library
 
 		//Lucas Montoya - Theke Solutions
 
 		//other
-		
+
 	];
 }
