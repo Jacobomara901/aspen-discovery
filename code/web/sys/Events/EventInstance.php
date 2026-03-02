7e068b2c422ff3df8155821ff9ca01e19b2a7099
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-03-02 13:19:35 +0000

    fix(api): generate visitable link for eventImageURL on event api return

--- code/web/sys/Events/EventInstance.php
+++ code/web/sys/Events/EventInstance.php
@@ -369,7 +369,10 @@ class EventInstance extends DataObject {
 		$recurrence = $event->getRecurrence();
 
 		global $configArray;
-		$bookingUrl = $configArray['Site']['url'] . '/AspenEvents/aspenEvents_' . $this->eventId . '_' . $this->id . '/Event';
+		$siteUrl = $configArray['Site']['url'];
+		$solrId = 'aspenEvents_' . $this->eventId . '_' . $this->id;
+		$bookingUrl = $siteUrl . '/AspenEvents/' . $solrId . '/Event';
+		$eventImageURL = $event->cover ? $siteUrl . '/bookcover.php?id=' . $solrId . '&size=medium&type=aspenEvent_event' : null;
 
 		return [
 			'id' => (int)$this->id,
@@ -388,7 +391,7 @@ class EventInstance extends DataObject {
 				'isFree' => true,
 				'venueName' => $venueName,
 				'organizer' => $organizer,
-				'eventImageURL' => $event->cover,
+				'eventImageURL' => $eventImageURL,
 				'tags' => $tags,
 				'isRecurring' => $recurrence !== null,
 				'recurrencePattern' => $recurrence['type'] ?? null,
