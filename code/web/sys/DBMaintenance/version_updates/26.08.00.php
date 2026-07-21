10873e0dcc130435d18d2ead724e27cc9505154a
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-07-21 17:06:50 +0100

    DIS-2276: Register BorrowBox module and admin permission

--- code/web/sys/DBMaintenance/version_updates/26.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.08.00.php
@@ -39,6 +39,61 @@ function getUpdates26_08_00(): array {
 
 		// stephen
 
+		//jacob
+		//borrowbox
+		'borrowbox_module' => [
+			'title' => 'Create BorrowBox Module',
+			'description' => 'Setup BorrowBox eContent integration module',
+			'sql' => [
+				"INSERT INTO modules (
+					name,
+					enabled,
+					indexName,
+					backgroundProcess,
+					logClassPath,
+					logClassName,
+					settingsClassPath,
+					settingsClassName
+				) VALUES (
+					'BorrowBox',
+					0,
+					'grouped_works',
+					'borrowbox_extract',
+					'/sys/BorrowBox/BorrowBoxExtractLogEntry.php',
+					'BorrowBoxExtractLogEntry',
+					'/sys/BorrowBox/BorrowBoxSetting.php',
+					'BorrowBoxSetting'
+				)",
+			]
+		], //borrowbox_module
+		'borrowbox_permissions' => [
+			'title' => 'BorrowBox Permissions',
+			'description' => 'Add permissions for BorrowBox administration',
+			'continueOnError' => false,
+			'sql' => [
+				"INSERT INTO permissions (
+					sectionName,
+					name,
+					requiredModule,
+					weight,
+					description
+				) VALUES (
+					'eContent - BorrowBox',
+					'Administer BorrowBox',
+					'BorrowBox',
+					10,
+					'Allows the user to administer BorrowBox integration settings.'
+				)",
+				"INSERT INTO role_permissions (
+					roleId,
+					permissionId
+				) VALUES (
+					(SELECT roleId from roles where name = 'opacAdmin'),
+					(SELECT id from permissions where name = 'Administer BorrowBox')
+				)",
+			]
+		], //borrowbox_permissions
+
 		//other
 
 	];
