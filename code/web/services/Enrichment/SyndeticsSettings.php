00113fb1301b334697a3b3045a3c8b8ca900690f
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-08-11 11:44:46 +0100

    covers migrate SyndeticsSettings admin to CoverProviderEditor

--- code/web/services/Enrichment/SyndeticsSettings.php
+++ code/web/services/Enrichment/SyndeticsSettings.php
@@ -1,10 +1,9 @@
 <?php
 
-require_once ROOT_DIR . '/Action.php';
-require_once ROOT_DIR . '/services/Admin/ObjectEditor.php';
+require_once ROOT_DIR . '/services/Enrichment/CoverProviderEditor.php';
 require_once ROOT_DIR . '/sys/Enrichment/SyndeticsSetting.php';
 
-class Enrichment_SyndeticsSettings extends ObjectEditor {
+class Enrichment_SyndeticsSettings extends Enrichment_CoverProviderEditor {
 	function getObjectType(): string {
 		return 'SyndeticsSetting';
 	}
@@ -13,69 +12,11 @@ class Enrichment_SyndeticsSettings extends ObjectEditor {
 		return 'SyndeticsSettings';
 	}
 
-	function getModule(): string {
-		return 'Enrichment';
-	}
-
 	function getPageTitle(): string {
 		return 'Syndetics Settings';
-	}
-
-	function getAllObjects(int $page, int $recordsPerPage): array {
-		$object = new SyndeticsSetting();
-		$object->limit(($page - 1) * $recordsPerPage, $recordsPerPage);
-		$this->applyFilters($object);
-		$object->orderBy($this->getSort());
-		$object->find();
-		$objectList = [];
-		while ($object->fetch()) {
-			$objectList[$object->id] = clone $object;
-		}
-		return $objectList;
-	}
-
-	function getDefaultSort(): string {
-		return 'id asc';
-	}
-
-	function getObjectStructure($context = ''): array {
-		return SyndeticsSetting::getObjectStructure($context);
-	}
-
-	function getPrimaryKeyColumn(): string {
-		return 'id';
-	}
-
-	function getIdKeyColumn(): string {
-		return 'id';
-	}
-
-	function getAdditionalObjectActions(?DataObject $existingObject): array {
-		return [];
-	}
-
-	function getInstructions(): string {
-		return 'https://aspen-discovery.atlassian.net/wiki/spaces/Help/pages/328105985/Third+Party+Enrichment';
-	}
-
-	function getBreadcrumbs(): array {
-		$breadcrumbs = [];
-		$breadcrumbs[] = new Breadcrumb('/Admin/Home', 'Administration Home');
-		$breadcrumbs[] = new Breadcrumb('/Admin/Home#third_party_enrichment', 'Third Party Enrichment');
-		$breadcrumbs[] = new Breadcrumb('/Enrichment/SyndeticsSettings', 'Syndetics Settings');
-		return $breadcrumbs;
-	}
-
-	function getActiveAdminSection(): string {
-		return 'third_party_enrichment';
-	}
-
-	public function getViewPermissions() : array {
-		return ['Administer Third Party Enrichment API Keys'];
 	}
 
 	function getInitializationJs(): string {
 		return 'AspenDiscovery.Admin.updateSyndeticsFields();';
 	}
-
 }
