332b1aa4ce29d190e6b3a7c106ed32b9e7f0e116
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-08-11 11:44:46 +0100

    covers migrate ContentCafeSettings admin to CoverProviderEditor

--- code/web/services/Enrichment/ContentCafeSettings.php
+++ code/web/services/Enrichment/ContentCafeSettings.php
@@ -1,10 +1,9 @@
 <?php
 
-require_once ROOT_DIR . '/Action.php';
-require_once ROOT_DIR . '/services/Admin/ObjectEditor.php';
+require_once ROOT_DIR . '/services/Enrichment/CoverProviderEditor.php';
 require_once ROOT_DIR . '/sys/Enrichment/ContentCafeSetting.php';
 
-class Enrichment_ContentCafeSettings extends ObjectEditor {
+class Enrichment_ContentCafeSettings extends Enrichment_CoverProviderEditor {
 	function getObjectType(): string {
 		return 'ContentCafeSetting';
 	}
@@ -13,68 +12,15 @@ class Enrichment_ContentCafeSettings extends ObjectEditor {
 		return 'ContentCafeSettings';
 	}
 
-	function getModule(): string {
-		return 'Enrichment';
-	}
-
 	function getPageTitle(): string {
 		return 'ContentCafe Settings';
 	}
 
-	function getAllObjects(int $page, int $recordsPerPage): array {
-		$object = new ContentCafeSetting();
-		$object->limit(($page - 1) * $recordsPerPage, $recordsPerPage);
-		$this->applyFilters($object);
-		$object->orderBy($this->getSort());
-		$object->find();
-		$objectList = [];
-		while ($object->fetch()) {
-			$objectList[$object->id] = clone $object;
-		}
-		return $objectList;
+	protected function getBreadcrumbLabel(): string {
+		return 'Content Cafe Settings';
 	}
 
-	function getDefaultSort(): string {
-		return 'id asc';
-	}
-
-	function getObjectStructure($context = ''): array {
-		return ContentCafeSetting::getObjectStructure($context);
-	}
-
-	function getPrimaryKeyColumn(): string {
-		return 'id';
-	}
-
-	function getIdKeyColumn(): string {
-		return 'id';
-	}
-
-	function getAdditionalObjectActions(?DataObject $existingObject): array {
-		return [];
-	}
-
-	function getInstructions(): string {
-		return 'https://aspen-discovery.atlassian.net/wiki/spaces/Help/pages/328105985/Third+Party+Enrichment';
-	}
-
-	function getBreadcrumbs(): array {
-		$breadcrumbs = [];
-		$breadcrumbs[] = new Breadcrumb('/Admin/Home', 'Administration Home');
-		$breadcrumbs[] = new Breadcrumb('/Admin/Home#third_party_enrichment', 'Third Party Enrichment');
-		$breadcrumbs[] = new Breadcrumb('/Enrichment/ContentCafeSettings', 'Content Cafe Settings');
-		return $breadcrumbs;
-	}
-
-	function getActiveAdminSection(): string {
-		return 'third_party_enrichment';
-	}
-
-	public function getViewPermissions() : array {
-		return ['Administer Third Party Enrichment API Keys'];
-	}
-
-	function canAddNew() : bool {
+	function canAddNew(): bool {
 		return $this->getNumObjects() == 0;
 	}
 }
