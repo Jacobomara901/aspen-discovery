bc0db11963c2dd7439c654e48b780d1f7cbc358b
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-08-11 11:44:46 +0100

    covers migrate SyndeticsSetting to LibraryScopedSetting trait

--- code/web/sys/Enrichment/SyndeticsSetting.php
+++ code/web/sys/Enrichment/SyndeticsSetting.php
@@ -1,7 +1,10 @@
 <?php /** @noinspection PhpMissingFieldTypeInspection */
 
+require_once ROOT_DIR . '/sys/DB/LibraryScopedSetting.php';
 
 class SyndeticsSetting extends DataObject {
+	use LibraryScopedSetting;
+
 	public $__table = 'syndetics_settings';    // table name
 	public $id;
 	public $name;
@@ -18,7 +21,9 @@ class SyndeticsSetting extends DataObject {
 	public $hasAuthorNotes;
 	public $hasVideoClip;
 
-	private $_libraries;
+	protected function getLibraryLinkColumn(): string {
+		return 'syndeticsSettingId';
+	}
 
 	public function getNumericColumnNames(): array {
 		return [
@@ -150,69 +155,4 @@ class SyndeticsSetting extends DataObject {
 		return self::$_objectStructure[$context];
 	}
 
-	public function __get($name) {
-		if ($name == "libraries") {
-			if (!isset($this->_libraries) && $this->id) {
-				$this->_libraries = [];
-				$obj = new Library();
-				$obj->syndeticsSettingId = $this->id;
-				$obj->find();
-				while ($obj->fetch()) {
-					$this->_libraries[$obj->libraryId] = $obj->libraryId;
-				}
-			}
-			return $this->_libraries;
-		} else {
-			return parent::__get($name);
-		}
-	}
-
-	public function __set($name, $value) {
-		if ($name == "libraries") {
-			$this->_libraries = $value;
-		} else {
-			parent::__set($name, $value);
-		}
-	}
-
-	public function update(string $context = '') : bool|int {
-		$ret = parent::update();
-		if ($ret !== FALSE) {
-			$this->saveLibraries();
-		}
-		return $ret;
-	}
-
-	public function insert(string $context = '') : int|bool {
-		$ret = parent::insert();
-		if ($ret !== FALSE) {
-			$this->saveLibraries();
-		}
-		return $ret;
-	}
-
-	public function saveLibraries() : void{
-		if (isset ($this->_libraries) && is_array($this->_libraries)) {
-			$libraryList = Library::getLibraryList(!UserAccount::userHasPermission('Administer All Libraries'));
-			foreach ($libraryList as $libraryId => $displayName) {
-				$library = new Library();
-				$library->libraryId = $libraryId;
-				$library->find(true);
-				if (in_array($libraryId, $this->_libraries)) {
-					//We want to apply the scope to this library
-					if ($library->syndeticsSettingId != $this->id) {
-						$library->syndeticsSettingId = $this->id;
-						$library->update();
-					}
-				} else {
-					//It should not be applied to this scope. Only change if it was applied to the scope
-					if ($library->syndeticsSettingId == $this->id) {
-						$library->syndeticsSettingId = -1;
-						$library->update();
-					}
-				}
-			}
-			unset($this->_libraries);
-		}
-	}
 }
