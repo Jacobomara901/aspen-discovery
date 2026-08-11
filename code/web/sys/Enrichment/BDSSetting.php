11d067a49dadf4cf00575510b65b150982b20afc
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-08-11 11:44:46 +0100

    covers migrate BDSSetting to LibraryScopedSetting trait

--- code/web/sys/Enrichment/BDSSetting.php
+++ code/web/sys/Enrichment/BDSSetting.php
@@ -1,15 +1,21 @@
 <?php /** @noinspection PhpMissingFieldTypeInspection */
 
+require_once ROOT_DIR . '/sys/DB/LibraryScopedSetting.php';
+
 class BDSSetting extends DataObject {
+	use LibraryScopedSetting;
+
 	public $__table = 'bds_settings';    // table name
 	public $id;
 	public $name;
 	public $dbmCode;
 	public $enabled;
 
-	private $_libraries;
-
 	static $_objectStructure = [];
+
+	protected function getLibraryLinkColumn(): string {
+		return 'bdsSettingId';
+	}
 
 	public function getEncryptedFieldNames() : array {
 		return [
@@ -67,72 +73,5 @@ class BDSSetting extends DataObject {
 
 		self::$_objectStructure[$context] = $structure;
 		return self::$_objectStructure[$context];
-	}
-
-	public function __get($name) {
-		if ($name !== "libraries") {
-			return parent::__get($name);
-		}
-		$needsLoad = !isset($this->_libraries) && $this->id;
-		if (!$needsLoad) {
-			return $this->_libraries ?? null;
-		}
-		$this->_libraries = [];
-		$obj = new Library();
-		$obj->bdsSettingId = $this->id;
-		$obj->find();
-		while ($obj->fetch()) {
-			$this->_libraries[$obj->libraryId] = $obj->libraryId;
-		}
-		return $this->_libraries;
-	}
-
-	public function __set($name, $value) {
-		if ($name !== "libraries") {
-			parent::__set($name, $value);
-			return;
-		}
-		$this->_libraries = $value;
-	}
-
-	public function update(string $context = '') : bool|int {
-		$ret = parent::update();
-		if ($ret === FALSE) {
-			return $ret;
-		}
-		$this->saveLibraries();
-		return $ret;
-	}
-
-	public function insert(string $context = '') : int|bool {
-		$ret = parent::insert();
-		if ($ret === FALSE) {
-			return $ret;
-		}
-		$this->saveLibraries();
-		return $ret;
-	}
-
-	public function saveLibraries() : void {
-		$hasPendingChanges = isset($this->_libraries) && is_array($this->_libraries);
-		if (!$hasPendingChanges) {
-			return;
-		}
-		$libraryList = Library::getLibraryList(!UserAccount::userHasPermission('Administer All Libraries'));
-		foreach ($libraryList as $libraryId => $displayName) {
-			$library = new Library();
-			$library->libraryId = $libraryId;
-			$library->find(true);
-			$needsLink = in_array($libraryId, $this->_libraries) && $library->bdsSettingId != $this->id;
-			$needsUnlink = !in_array($libraryId, $this->_libraries) && $library->bdsSettingId == $this->id;
-			if ($needsLink) {
-				$library->bdsSettingId = $this->id;
-				$library->update();
-			} elseif ($needsUnlink) {
-				$library->bdsSettingId = -1;
-				$library->update();
-			}
-		}
-		unset($this->_libraries);
 	}
 }
