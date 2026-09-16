8288cabb8248891219aa9b6a3704785e9d7231cb
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-09-16 16:16:48 +0100

    DIS-894: Add javascript for the Omeka record page

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -15348,6 +15348,31 @@ AspenDiscovery.OpenArchives = (function () {
 		}
 	};
 }(AspenDiscovery.OpenArchives || {}));
+AspenDiscovery.Omeka = (function () {
+	return {
+
+		getStaffView: function (id) {
+			var url = Globals.path + "/Omeka/" + id + "/AJAX?method=getStaffView";
+			$.getJSON(url, function (data) {
+				if (!data.success) {
+					AspenDiscovery.showMessage('Error', data.message);
+				} else {
+					$("#staffViewPlaceHolder").replaceWith(data.staffView);
+				}
+			});
+			return false;
+		},
+
+		getLargeCover: function (id) {
+			var url = Globals.path + "/Omeka/" + id + "/AJAX?method=getLargeCover";
+			$.getJSON(url, function (data) {
+				AspenDiscovery.showMessageWithButtons(data.title, data.modalBody, data.modalButtons);
+			});
+			return false;
+		}
+	}
+}(AspenDiscovery.Omeka || {}));
+
 AspenDiscovery.Hoopla = (function(){
 	return {
 		checkOutHooplaTitle: function (hooplaId, patronId, hooplaType) {
